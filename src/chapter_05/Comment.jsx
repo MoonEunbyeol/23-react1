4ffb56591f1e55a1b89d47fcfaaec2f7cdd61917
MoonEunbyeol <202130114@email.daelim.ac.kr>
2023-04-06 15:39:48 +0900

    Update Comment.jsx, CommentList.jsx
    Comment.jsx, CommentList.jsx 컴포넌트 추가 및 주석 추가

--- src/chapter_05/Comment.jsx
+++ src/chapter_05/Comment.jsx
@@ -38,15 +38,16 @@ function Comment(props) {
       <div style={styles.imageContainer}>
         <img
           src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
+          // src="./image/picture.png" 이런 식으로도 추가 가능. index 기준으로 작성
           alt="프로필 이미지"
           style={styles.image}
         />
       </div>
       <div style={styles.contentContainer}>
-        <span style={styles.nameText}>문은별</span>
-        <span style={styles.commentText}>
-          제가 만든 첫 컴포넌트입니다. (5.6 실습)
-        </span>
+        {/* <span style={styles.nameText}>문은별</span>
+        <span style={styles.commentText}>제가 만든 첫 컴포넌트입니다. (5.6 실습)</span> */}
+        <span style={styles.nameText}> {props.name} </span>
+        <span style={styles.commentText}> {props.comment} </span> {/*props로 전달받는 경우*/}
       </div>
     </div>
   );
--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,12 +1,42 @@
 import React from "react";
 import Comment from "./Comment";
 
+const comments = [
+  {
+    name: "문은별",
+    comment: "안녕하세요. 문은별입니다."
+  },
+  {
+    name: "홍길동",
+    comment: "안녕하세요. 홍길동입니다."
+  },
+  {
+    name: "테스트",
+    comment: "안녕하세요. 테스트입니다."
+  },
+]
+
 function CommentList(props) {
   return (
     <div>
-      <Comment />
+      {comments.map((foo) => {
+        return (
+          <Comment name={foo.name} comment={foo.comment} />
+        )
+      })}
     </div>
-  );
+  )
 }
+
+// function CommentList(props) {
+//   return (
+//     <div>
+//       {/* <Comment /> */}
+//       <Comment name={"문은별"} comment={"안녕하세요. 문은별입니다."} />
+//       <Comment name={"홍길동"} comment={"안녕하세요. 홍길동입니다."} />
+//       <Comment name={"성이름"} comment={"안녕하세요. 성이름입니다."} />
+//     </div>
+//   );
+// }
 
 export default CommentList; //세미콜론 없어도 오류 미발생
